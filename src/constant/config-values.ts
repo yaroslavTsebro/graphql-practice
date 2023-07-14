82f0e65dbecda355c2310b6cc76401c734d9bf4b
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-07-14 16:06:30 +0300

    added constants

--- src/constant/config-values.ts
+++ src/constant/config-values.ts
@@ -1,4 +1,5 @@
 export class ConfigValues{
   public static readonly CONFIG_PATH = "../config/";
   public static readonly ENV_EXTENSION = '.env.';
+  public static readonly DEVELOPMENT_EVN = ConfigValues.ENV_EXTENSION + 'development';
 }
---
+++ src/constant/default-config-values.ts
@@ -0,0 +1,7 @@
+export class DefaultConfigValues {
+  public static readonly DB_HOST = "localhost";
+  public static readonly DB_NAME = "database";
+  public static readonly DB_USER = "postrges";
+  public static readonly DB_PASSWORD = "postrges";
+  public static readonly DB_PORT = "5432";
+}
