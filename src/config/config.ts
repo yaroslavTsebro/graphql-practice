0dbe98de39bd0c497f2d378b392f8512adfbf9e3
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-07-14 16:06:55 +0300

    added configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,31 @@
 import {ConfigValues} from "../constant/config-values";
 import * as fs from "fs";
+import dotenv from "dotenv";
+import {DefaultConfigValues} from "../constant/default-config-values";
 
+const env = process.env.NODE_ENV;
 fs.access(
     ConfigValues.CONFIG_PATH +
     ConfigValues.ENV_EXTENSION +
-    process.env.NODE_ENV, (err) => {
+    env,
+    (err) => {
       dotenv.config(
           {path: ConfigValues.CONFIG_PATH + ConfigValues.DEVELOPMENT_EVN})
-    })
+    }
+)
 dotenv.config({
   path:
       ConfigValues.CONFIG_PATH +
       ConfigValues.ENV_EXTENSION +
-      process.env.NODE_ENV
+      env
 });
+
+export const config = {
+  db: {
+    DB_HOST: process.env.DB_HOST || DefaultConfigValues.DB_HOST,
+    DB_NAME: process.env.DB_NAME || DefaultConfigValues.DB_NAME,
+    DB_USER: process.env.DB_USER || DefaultConfigValues.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD || DefaultConfigValues.DB_PASSWORD,
+    DB_PORT: process.env.DB_PORT || DefaultConfigValues.DB_PORT,
+  }
+}
