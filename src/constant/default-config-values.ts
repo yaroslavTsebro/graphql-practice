395fa155a28606f222cd5a51c2aa3f3ee68530a5
Yaroslav Tsebro <88938087+yaroslavTsebro@users.noreply.github.com>
2023-07-18 09:01:05 +0300

    Update default-config-values.ts

--- src/constant/default-config-values.ts
+++ src/constant/default-config-values.ts
@@ -2,7 +2,7 @@ export class DefaultConfigValues {
   public static readonly DB_HOST = "localhost"; // host.docker.internal
   public static readonly DB_NAME = "database";
   public static readonly DB_USER = "postgres";
-  public static readonly DB_PASSWORD = "zsbldqpk56";
+  public static readonly DB_PASSWORD = "***";
   public static readonly DB_TYPE = "postgresql";
   public static readonly DB_PORT = 5432;
   public static readonly APP_PORT = 3000;
